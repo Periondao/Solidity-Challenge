8265577661499ba4b4fddd20d092486561dab051
Itahand Naizir <itahand.naizir@gmail.com>
2022-08-23 20:59:02 -0500

    Unstaking functionality with tests.
    9 tests passed.

--- ERC20Pool-Staking/contracts/ERC20Pool.sol
+++ ERC20Pool-Staking/contracts/ERC20Pool.sol
@@ -24,11 +24,13 @@ contract ERC20Pool is Pausable, Ownable, ReentrancyGuard {
     }
 
   event Staked(address indexed from, uint256 amount);
+  event Unstaked(address indexed from, uint256 amount);
 
   // 7 Days (7 * 24 * 60 * 60)
   uint256 public rewardDuration = 604800;
 
-  uint8 public totalStakers;
+  uint256 public totalStakers;
+  uint256 public totalStaked;
 
   mapping(address => StakeInfo) public stakeInfos;
   mapping(address => bool) public addressStaked;
@@ -37,17 +39,18 @@ contract ERC20Pool is Pausable, Ownable, ReentrancyGuard {
         require(address(_tokenAddress) != address(0),"Token Address cannot be address 0");
         perionToken = _tokenAddress;
         totalStakers = 0;
+        totalStaked = 0;
     }
 
-
-
+    // Staking functionality
     function stakeToken(uint256 stakeAmount) external payable whenNotPaused {
-        require(stakeAmount >0, "Stake amount should be correct");
+        require(stakeAmount > 0, "Stake amount should be correct");
         require(addressStaked[_msgSender()] == false, "You already participated");
         require(perionToken.balanceOf(_msgSender()) >= stakeAmount, "Insufficient Balance");
 
            perionToken.transferFrom(_msgSender(), address(this), stakeAmount);
             totalStakers++;
+            totalStaked = totalStaked + stakeAmount;
             addressStaked[_msgSender()] = true;
 
             stakeInfos[_msgSender()] = StakeInfo({
@@ -60,6 +63,21 @@ contract ERC20Pool is Pausable, Ownable, ReentrancyGuard {
         emit Staked(_msgSender(), stakeAmount);
     }
 
+    // Unstaking functionality
+    function unstakeToken() external returns (bool){
+        require(addressStaked[_msgSender()] == true, "You have no staked token");
+        require(stakeInfos[_msgSender()].claimed == 0, "Already unstaked!");
+
+        uint256 stakeAmount = stakeInfos[_msgSender()].amount;
+        stakeInfos[_msgSender()].claimed == stakeAmount;
+        perionToken.transfer(_msgSender(), stakeAmount);
+        totalStaked = totalStaked - stakeAmount;
+
+
+        emit Unstaked(_msgSender(), stakeAmount);
+
+        return true;
+    }
 
     function pause() external onlyOwner {
         _pause();
--- ERC20Pool-Staking/test/ERC20Pool.js
+++ ERC20Pool-Staking/test/ERC20Pool.js
@@ -9,7 +9,7 @@ const tokens = (n) => {
 describe('ERC20Pool', () => {
   // Assigning global variables inside the function.
 
-  let team, user1
+  let team, user1, user2
 
   beforeEach(async () => {
     const ERC20Pool = await ethers.getContractFactory('ERC20Pool')
@@ -20,6 +20,7 @@ describe('ERC20Pool', () => {
     accounts = await ethers.getSigners()
     team = accounts[0]
     user1 = accounts[1]
+    user2 = accounts[2]
 
     let transaction = await perion.connect(team).transfer(user1.address, tokens(100))
     await transaction.wait()
@@ -31,6 +32,10 @@ describe('ERC20Pool', () => {
 
     it('Tracks total stakers.', async () => {
       expect(await pool.totalStakers()).to.equal(0)
+    })
+
+    it('Tracks total of staked tokens.', async () => {
+      expect(await pool.totalStaked()).to.equal(0)
     })
 
     it('The owner is the deployer', async () => {
@@ -56,9 +61,9 @@ describe('ERC20Pool', () => {
 
       })
 
-      it('tracks the stake transaction info', async () => {
+      it('tracks total stakers and total token staked', async () => {
         expect(await pool.totalStakers()).to.equal(1)
-
+        expect(await pool.totalStaked()).to.equal(amount)
       })
 
       it('emits a Staked event', async () => {
@@ -70,6 +75,59 @@ describe('ERC20Pool', () => {
         expect(args.amount).to.equal(amount)
       })
 
+    })
+
+    describe('Failure', () => {
+      it('fails when no tokens are approved', async () => {
+        // Don't approve any tokens before depositing
+        await expect(pool.connect(user1).stakeToken(amount)).to.be.reverted
+      })
+    })
+
+  })
+
+  describe('Unstaking Tokens', () => {
+    let transaction, result
+    let stake = tokens(20)
+
+    beforeEach(async () => {
+      // Approve amount
+      transaction = await perion.connect(user1).approve(pool.address, stake)
+      result = await transaction.wait()
+
+      // Connect user1 with the pool contract and stake
+      transaction = await pool.connect(user1).stakeToken(stake)
+      result = await transaction.wait()
+
+      // Connect user1 with the pool contract and unstake his tokens
+      transaction = await pool.connect(user1).unstakeToken()
+      result = await transaction.wait()
+    })
+
+    describe('Success', () => {
+
+      it('tracks total stakers and total token staked', async () => {
+        expect(await pool.totalStakers()).to.equal(1)
+        expect(await pool.totalStaked()).to.equal(0)
+      })
+
+      it('emits a Unstaked event', async () => {
+        const event = result.events[2] // It emits three events
+        expect(event.event).to.equal('Unstaked')
+
+        const args = event.args
+        expect(args.from).to.equal(user1.address)
+        expect(args.amount).to.equal(stake)
+      })
+
+    })
+
+    describe('Failure', () => {
+
+      it('fails when the user has no tokens', async () => {
+        // Don't approve any tokens before depositing
+        await expect(pool.connect(user2).unstakeToken()).to.be.reverted
+      })
     })
 
   })
